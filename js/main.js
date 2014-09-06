9842a08a371a0dd108f7a81dcd033a3ac8779018
developfor <will@developfor.com>
2014-09-06 12:47:29 -0400

    updated app

--- js/main.js
+++ js/main.js
@@ -10,9 +10,13 @@ var map;
 			   +'<p><span>Gender: </span>'+d["Gender"]+ '</p>'
 			   +'<p><span>Project Website: </span><a href="http://'+d["Project Website"]+'">'+d["Project Website"]+ '</a></p>'
 			   +'<p><span>Project Title: </span>'+d["Project Title"]+ '</p>'
-			   +'<p ><span>Project LatLong: </span>'+d["Project Location"]+ '<br /><a href="#" id="zoom_map"> View Location On Map.</a></p>'
+			   +'<p><span>Project Start Date: </span>'+d["Project Start Date"]+ '</p>'
+			   +'<p><span>Project End Date: </span>'+d["Project End Date"]+ '</p>'
+			   +'<p><span>Is the project ongoing?: </span>'+d["Is the project ongoing?"]+ '</p>'
+			   +'<p ><span>Project LatLong: </span>'+d["Project Location"]+ '<br /><a id="zoom_map"> View Location On Map.</a></p>'
    			   +'<p><span>Project Country: </span>'+d["Country"]+ '</p>'
 			   +'<p><span>Project Description: </span>'+d["Project Description"]+ '</p>'
+			   +'<p><span>Impact Description: </span>'+d["Impact Description"]+ '</p>'
 			   +'</div>';
 			}
 
@@ -59,9 +63,10 @@ var map;
 
 	$.ajax({ 
 	    type: 'GET', 
-	    // url: 'https://www.googleapis.com/fusiontables/v1/query?sql=SELECT*%20FROM%201uPQIxXfKAdTZuhBMu-jSkdyJs45o7VC2TGqm17Eg&key=AIzaSyD0ojLPNCk0yzMoMqSJ94hivxVQI64yAAw', 
-	    // url: 'js/query.json', 
-	    url: 'api.php',
+
+	    url: 'https://www.googleapis.com/fusiontables/v1/query?sql=SELECT*%20FROM%201kWGRT8isIwfwo0xZyh_g48D71Dgn_cKS51FC1rLu&key=AIzaSyC_NLU85fZGzJJVSX2vt5JZcMHbxhfNXH0', 
+	    // url: 'js/query.json',
+	    // url: 'api.php',
 	    data: { get_param: 'value' }, 
 	    dataType: 'json',
 		cache: false,
@@ -75,15 +80,17 @@ var map;
 					var item = {
 				    	"Timestamp" : data["rows"][index][0],
 					    "Name": data["rows"][index][1],
-					    // "Age": data["rows"][index][2],
-					    // "Email Address": data["rows"][index][3],
 						"Gender": data["rows"][index][2],
 						"Project Title": data["rows"][index][3],
 						"Project Location": data["rows"][index][4],
-						"Project Description": data["rows"][index][5],
-						"Project Website": data["rows"][index][6],
-						"latlng": data["rows"][index][7],
-						"Country": data["rows"][index][8]
+						"Country": data["rows"][index][5],
+						"Project Description": data["rows"][index][6],
+						"Impact Description": data["rows"][index][7],
+						"Project Website": data["rows"][index][8],
+						"Project Start Date": data["rows"][index][9],
+						"Is the project ongoing?": data["rows"][index][10],
+						"Project End Date": data["rows"][index][11],
+						"LatLng": data["rows"][index][12]
 						}
 					array.push(item);
 		        }  
